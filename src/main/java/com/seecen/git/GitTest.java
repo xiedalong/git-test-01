6e50ef98e2ddf6b6679135c1e2f21e3edc173358
xiedalong <1492263193@qq.com>
2022-07-27 09:41:09 +0800

    first commit

---
+++ src/main/java/com/seecen/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.seecen.git;
+
+/**
+ * @program: git-test-01
+ * @description:
+ * @author: 谢达龙
+ * @create: 2022-07-27 09:37
+ * @version:1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+
+
+}
